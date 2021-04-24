8a313f4402e59994e32f1f0071880d95657467bb
DuyPhong <nguyenduyphong1504@gmail.com>
2021-04-24 12:09:14 +0700

    login done

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,24 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Landing from './components/Landing'
-import  Auth from './view/Auth'
+import Auth from './view/Auth'
+import AuthContextProvider from './context/AuthContext'
+
+
 
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Landing}></Route>
-        <Route exact path='/login' render={props => <Auth {...props} authRoute='login'></Auth>} />
-        <Route exact path='/register' render={props => <Auth {...props} authRoute='register'></Auth>} />
-      </Switch>
-  
-    </Router >
+    <AuthContextProvider>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Landing}></Route>
+          <Route exact path='/login' render={props => <Auth {...props} authRoute='login'></Auth>} />
+          <Route exact path='/register' render={props => <Auth {...props} authRoute='register'></Auth>} />
+        </Switch>
+      </Router >
+    </AuthContextProvider>
+
   );
 }
 
--- client/src/components/auth/LoginForm.js
+++ client/src/components/auth/LoginForm.js
@@ -1,29 +1,57 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
+import { useState, useContext } from 'react'
+import { AuthContext } from "../../context/AuthContext"
+
+
 
 const Login = () => {
-    return (
-        <div className='container col-5'>
-            <div>
-                <Form.Group>
-                    <Form.Control type="text" placeholder="Username" name="username" required>
-                    </Form.Control>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Control type="password" placeholder="password" name="password" required>
-                    </Form.Control>
-                </Form.Group>
-                <Button variant="success" type="submit">Login</Button>
-            </div>
-            <p>Dont have an account?  </p>
-            <Link to="/register" >
-                <Button variant="info" size="sm" classname="ml-2">Register</Button>
-            </Link>
+    const { loginUser } = useContext(AuthContext)
 
+    const [loginForm, setLoginForm] = useState({
+        username: '',
+        password: ''
+    })
+
+    const { username, password } = loginForm
+
+    const onChangeLoginForm = event => setLoginForm({ ...loginForm, [event.target.name]: event.target.value })
+
+
+    const login = async event => {
+        event.preventDefault()
+        try {
+            const loginData = await loginUser(loginForm)
+            console.log(loginData)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+
+return (
+    <Form className='container col-5' onSubmit={login}>
+        <div>
+            <Form.Group>
+                <Form.Control type="text" placeholder="Username" name="username" required value={username} onChange={onChangeLoginForm}>
+                </Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Control type="password" placeholder="password" name="password" required value={password} onChange={onChangeLoginForm} >
+                </Form.Control>
+            </Form.Group>
+            <Button variant="success" type="submit">Login</Button>
         </div>
+        <p>Dont have an account?  </p>
+        <Link to="/register" >
+            <Button variant="info" size="sm" classname="ml-2">Register</Button>
+        </Link>
 
-    )
+    </Form>
+
+)
 }
 
 export default Login
